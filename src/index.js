22fe11d7bca79a52ad8c296f4f080098f7db860e
jcalahi <juliuscalahi@gmail.com>
2023-08-13 13:32:30 +0800

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const router = express.Router();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
